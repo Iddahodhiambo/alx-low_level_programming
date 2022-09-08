660756162ecf4205eaf156f9b7533b958252f9a3
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-09-08 10:03:33 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- * Return:Always 0 (Success)
- */
-
 int main()
 {
 	puts("Programming is like building a multilingual puzzle\n");
+
 	return 0;
 }	
