1bc4ac6736ab5a6fa46e349816d8137056009bb8
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-09-12 07:04:38 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h)
+
+/**
+ * main - print alphabet reverse using putchar
+ * Return:always 0 (sucess)
+ */
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0)
