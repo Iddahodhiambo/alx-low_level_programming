afef7599c9e07835d30b5c8439592a8faa6802f2
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-09-12 06:47:40 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print 0123456789 using putchar
+ * Return:always o (success)
+ */
+
+int main(void)
+{
+	int n = '0';
+	while (n <= '9')
+	{
+		putcha(n);
+		n++;
+	}
+	putchar('\n');
+	return (0)
