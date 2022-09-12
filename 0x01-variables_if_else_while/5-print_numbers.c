7d29ab6256fd195ecf17c15fc48b86062150cef1
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-09-12 07:58:38 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
  * main - print 0123456789
- *
  * Description:using the main function
- * this program prints "programming is positive,zero,or negative
  * return: 0
  */
 int main(void)
