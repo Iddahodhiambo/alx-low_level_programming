22d9a59ff3e03bbac5f2726f3d14e9c9e062469d
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-09-14 10:22:28 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - function that computes the absolutr value of an integer
+ * @c:integer to be used in the argument of function
+ * Return: 0
+ */
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+		return (c* -1);
+}
+
