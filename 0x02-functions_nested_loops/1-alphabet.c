e50bc7d3f831965087418064fc916605e5d9025e
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-09-14 06:51:15 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * print alphabet - function to print abc
+ *
+ * Retur: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+}
