3dd219d7e00d8bc66478360380514812d8c8356b
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-09-14 13:38:00 -0700

    task 8

---
+++ 0x02-functions_nested_loops/100-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @num 1: The first integer to be added
+ * @num 2: The second integer to be added
+ *
+ * Return: The result of the addition\
+ */
+int add(int num1,int num2)
+{
+	return (num1 + num2)
+}
