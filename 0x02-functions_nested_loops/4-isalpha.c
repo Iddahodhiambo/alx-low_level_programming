0382aeeb83c02b30fa105de4d01ffedb817a3b90
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-09-14 08:23:09 -0700

    task 3

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+# include "main.h"
+/**
+ * _isalpah - function to check if character is lowercase or uppercase
+ * @c - int to be used for the argument of the function
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a'; && c <= 'z') || (c >= 'A'; && c <= 'Z'))
+	{
+		Return (1);
+	}
+	else
+		Return (0);
+}
