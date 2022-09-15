7e1b4189767e6f1a560fd17c99defada4ca16696
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-09-15 00:15:26 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	int i, res;
 
-	for (i = 0; 1 < 1024; i++)
-		if (i % 3 == 0 || i % 5 == 0)
+	for (i = 0; i < 1024; i++)
+		if ((i % 3 == 0) || (i % 5 == 0))
 			res += 1;
 	printf("%d\n", res);
 	return (0);
