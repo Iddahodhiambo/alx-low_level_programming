1934daa634c5ff11f6f8d4bcf947a9c5bdade627
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-09-28 11:51:49 -0700

    task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * is_palindrome - returns the 1 if s is a palindrome
+ * @s: string to be checked
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int flag = 1;
+
+	check(s, 0, strlen_recursion(s) - 1. &flag);
+	return (flag);
+}
+/**
+ * check - checks if a string is palindrome
+ * @s: string to be checked
+ * @start: start index
+ * @end: end index
+ * @flag: to indicate if a string is palindrome
+ *
+ * Return: void
+ */
+void check(char *s, int start, int end, int *flag)
+{
+	if (start <= end)
+	{
+		if (s[start] == s[end])
+			*flag *= -1;
+		else
+			*flag *= 0;
+		check(s,start + 1, end - 1, flag);
+	}
+}
