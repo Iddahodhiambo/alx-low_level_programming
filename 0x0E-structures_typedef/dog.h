95abffdd91815da4ea0719aefae4693dfeb101cf
Iddahodhiambo <Iddahawuor@yahoo.com>
2022-10-11 03:46:53 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: this struct is for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif /*DOG_H*/
